137ff99ad6a90a7469fb9d4f0748782e8e9633bf
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-08-12 15:59:06 +0300

    fix(routes): rename ProtectedRoute to BusinessRoute, ProfileCompleteRoute, and WorkerRoute for clarity

--- src/guards/BusinessRoute.jsx
+++ src/guards/BusinessRoute.jsx
@@ -1,14 +1,22 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
-function ProtectedRoute({ children }) {
+function BusinessRoute({ children }) {
     const {loading, user} = useAuth()
 
 
     if(loading) return <p>Loading...</p>
 
+    if (!user) {
+        return <Navigate to='/sign-in' />;
+    }
+
     if (user?.role !== 'business') {
         return <Navigate to="/" />;
+    }
+
+    if (!user.isProfileComplete) {
+        return <Navigate to='/profile/form' />;
     }
 
 
@@ -16,4 +24,4 @@ function ProtectedRoute({ children }) {
 }
 
 
-export default ProtectedRoute;
+export default BusinessRoute;
